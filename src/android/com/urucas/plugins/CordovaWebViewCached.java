0618fdce010fd84b493188e9e35b009eb03cdb54
ShadowEnforcer <Andy.Ritter12@web.de>
2020-03-29 20:23:20 +0200

    Update CordovaWebViewCached.java

--- src/android/com/urucas/plugins/CordovaWebViewCached.java
+++ src/android/com/urucas/plugins/CordovaWebViewCached.java
@@ -22,9 +22,10 @@ public class CordovaWebViewCached extends IceCreamCordovaWebViewClient{
 	@Override
 	public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
 
-		if(url.endsWith(".jpg") || url.endsWith(".jpeg") || url.endsWith(".png") || url.endsWith(".gif")){
+		if(url.endsWith(".jpg") || url.endsWith(".jpeg") || url.endsWith(".png") || url.endsWith(".gif") || url.endsWith(".mp4") ||
+		   url.endsWith(".avi") || url.endsWith(".flv") || url.endsWith(".wmv") || url.endsWith(".mov") || url.endsWith(".webm")){
 			try {
-				Log.i(TAG, "Overriding image url load");
+				Log.i(TAG, "Overriding file url load");
 				Log.i(TAG, url);
 
 				ImageCache _cache = ImageCachePlugin.getCache();
@@ -35,6 +36,18 @@ public class CordovaWebViewCached extends IceCreamCordovaWebViewClient{
 					_cache.register(url, cacheFileName, "image/png", "UTF-8", ImageCachePlugin.CACHE_TIME);
 				}else if(url.endsWith(".gif")){
 					_cache.register(url, cacheFileName, "image/gif", "UTF-8", ImageCachePlugin.CACHE_TIME);
+				}else if(url.endsWith(".mp4")){
+					_cache.register(url, cacheFileName, "video/mp4", "UTF-8", ImageCachePlugin.CACHE_TIME);
+				}else if(url.endsWith(".avi")){
+					_cache.register(url, cacheFileName, "video/avi", "UTF-8", ImageCachePlugin.CACHE_TIME);
+				}else if(url.endsWith(".flv")){
+					_cache.register(url, cacheFileName, "video/flv", "UTF-8", ImageCachePlugin.CACHE_TIME);
+				}else if(url.endsWith(".wmv")){
+					_cache.register(url, cacheFileName, "video/wmv", "UTF-8", ImageCachePlugin.CACHE_TIME);
+				}else if(url.endsWith(".mov")){
+					_cache.register(url, cacheFileName, "video/mov", "UTF-8", ImageCachePlugin.CACHE_TIME);
+				}else if(url.endsWith(".webm")){
+					_cache.register(url, cacheFileName, "video/webm", "UTF-8", ImageCachePlugin.CACHE_TIME);
 				}
 				return _cache.load(url);
 				
